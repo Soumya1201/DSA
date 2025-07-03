2a62ecfc38d9aa192050953e7d3c9651720093e1
Soumyadip Adhikary <145369075+Soumya1201@users.noreply.github.com>
2025-07-03 22:10:57 +0530

    Time: 0 ms (100.00%) | Memory: 26.1 MB (67.08%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.cpp
@@ -6,7 +6,7 @@ public:
         if(nums[0] != nums[1]) return nums[0];
         if(nums[n-1] != nums[n-2]) return nums[n-1];
 
-        int low = 0, high = n - 1;
+        int low = 1, high = n - 2;
         while(low <= high)
         {
             int mid = low + (high - low) / 2;
