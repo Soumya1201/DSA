a00b10f8385d14dff98b93f701a61f7b4a641dd0
Soumyadip Adhikary <145369075+Soumya1201@users.noreply.github.com>
2025-07-04 08:28:16 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Number of occurrence/number-of-occurrence.cpp
+++ Difficulty: Easy/Number of occurrence/number-of-occurrence.cpp
@@ -1,28 +1,30 @@
 int firstOcc(vector<int>& arr, int target)
 {
-    int low = 0, high = arr.size(), ans = -1;
+    int low = 0, high = arr.size()-1;
+    int ans = -1;
     while(low <= high)
     {
         int mid = low + (high - low) / 2;
-        if(arr[mid] == target) {
+        if(arr[mid] == target)
+        {
             ans = mid;
             high = mid - 1;
         }
-        else if(arr[mid] < target)
+        else if(arr[mid] > target)
         {
-            low = mid + 1;
+            high = mid -1;
         }
-        else
-        {
-            high = mid - 1;
-        }
-
+        else {
+           low = mid + 1;
+       }
     }
     return ans;
 }
+
 int lastOcc(vector<int>& arr, int target)
 {
-    int low = 0, high = arr.size(), ans = -1;
+    int low = 0, n = arr.size(), high = n-1;
+    int ans = -1;
     while(low <= high)
     {
         int mid = low + (high - low) / 2;
@@ -31,28 +33,25 @@ int lastOcc(vector<int>& arr, int target)
             ans = mid;
             low = mid + 1;
         }
-        else if(arr[mid] < target)
+        else if(arr[mid] > target)
         {
-            low = mid + 1;
+            high = mid -1;
         }
         else
         {
-            high = mid - 1;
+            low = mid + 1;
         }
     }
     return ans;
-    
 }
-
 class Solution {
   public:
     int countFreq(vector<int>& arr, int target) {
         // code here
-        int a = firstOcc(arr, target);
-        if(a == -1) return 0;
-        
-        int b = lastOcc(arr, target);
-        
-        return (b-a)+1;
+        int n = arr.size();
+        int lb = firstOcc(arr, target);
+        if(lb == -1) return 0;
+        int ub = lastOcc(arr, target);
+        return (ub-lb +1);;
     }
 };
