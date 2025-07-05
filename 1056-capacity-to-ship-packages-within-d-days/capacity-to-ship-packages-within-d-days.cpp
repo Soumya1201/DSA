0822ee1296e03a31ac9a66d1494c745a163505af
Soumyadip Adhikary <145369075+Soumya1201@users.noreply.github.com>
2025-07-05 11:10:20 +0530

    Time: 11 ms (68.62%) | Memory: 34.9 MB (66.04%) - LeetSync

---
+++ 1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -0,0 +1,52 @@
+int findDay(vector<int>& weights, int capacity)
+{
+    int day = 1, load = 0;
+    int n = weights.size();
+    for(int i = 0; i<n; i++)
+    {
+        if(load + weights[i] > capacity)
+        {
+            day = day + 1;
+            load = weights[i];
+        }
+        else
+        {
+            load += weights[i];
+        }
+    }
+    return day;
+}
+class Solution {
+public:
+    int shipWithinDays(vector<int>& weights, int days) {
+        int n = weights.size();
+        int ans = -1;
+        int low = INT_MIN;
+        for(int i = 0; i<n; i++)
+        {
+            low = max(low, weights[i]);
+        }
+        int high = 0;
+        for(int i = 0; i<n; i++)
+        {
+            high += weights[i];
+        }
+
+        while(low <= high)
+        {
+            int mid = low + (high - low) / 2;    //capacity
+            int day = findDay(weights, mid);
+
+            if(day <= days)
+            {
+                ans = mid;
+                high = mid - 1;
+            }
+            else
+            {
+                low = mid + 1;
+            }
+        }
+        return ans;
+    }
+};
