e7227abb572377ee918276a9475aaaddb0160197
Soumyadip Adhikary <145369075+Soumya1201@users.noreply.github.com>
2025-07-06 12:34:08 +0530

    Time: 0 ms (100.00%) | Memory: 96 MB (18.75%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.cpp
@@ -1,24 +1,29 @@
 class Solution {
-public:
-    double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
-        int n1 = nums1.size();
-        int n2 = nums2.size();
+    public:
+        double findMedianSortedArrays(vector<int>& nums1, vector<int>& nums2) {
+                int n1 = nums1.size();
+                        int n2 = nums2.size();
+                                int i = 0, j = 0;
+                                        vector<int> nums;
 
-        vector<int> nums;
-        for(int i = 0; i<n1; i++)
-        {
-            nums.push_back(nums1[i]);
-        }
-        for(int i = 0; i<n2; i++)
-        {
-            nums.push_back(nums2[i]);
-        }
-        
+                                                while (i < n1 && j < n2) {
+                                                            if (nums1[i] < nums2[j])
+                                                                            nums.push_back(nums1[i++]);
+                                                                                        else
+                                                                                                        nums.push_back(nums2[j++]);
+                                                                                                                }
 
-        sort(nums.begin(), nums.end());
+                                                                                                                        while (i < n1)
+                                                                                                                                    nums.push_back(nums1[i++]);
 
-        int n = nums.size();
-        if(n % 2 == 1) return (double)((double)(nums[n/2]));
-        else return (double)((double)(nums[n/2]) + (double)(nums[n/2 - 1])) / 2.0;
-    }
-};
+                                                                                                                                            while (j < n2)
+                                                                                                                                                        nums.push_back(nums2[j++]);
+
+                                                                                                                                                                int n = nums.size();
+                                                                                                                                                                        if (n % 2 == 1)
+                                                                                                                                                                                    return (double)(nums[n / 2]);
+                                                                                                                                                                                            else
+                                                                                                                                                                                                        return ((double)(nums[n / 2]) + (double)(nums[n / 2 - 1])) / 2.0;
+                                                                                                                                                                                                            }
+                                                                                                                                                                                                            };
+
