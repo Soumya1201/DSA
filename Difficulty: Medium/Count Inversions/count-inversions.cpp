fe17b1a670f386d0eb0bdadd4b0706b68e990fd7
Soumyadip Adhikary <145369075+Soumya1201@users.noreply.github.com>
2025-07-15 20:08:37 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Count Inversions/count-inversions.cpp
@@ -0,0 +1,58 @@
+int merge(vector<int> &arr, int low, int mid, int high)
+{
+    int left = low;
+    int count = 0;
+    int right = mid + 1;
+    vector<int> temp;
+    while(left <= mid && right <= high)
+    {
+        if(arr[left] <= arr[right])
+        {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else
+        {
+            count += mid - left + 1;
+            temp.push_back(arr[right]);
+            right++;
+        }
+    }
+    while(left <= mid)
+    {
+        temp.push_back(arr[left]);
+        left++;
+    }
+    while(right <= high)
+    {
+        temp.push_back(arr[right]);
+        right++;
+    }
+    for(int i = low; i<= high; i++)
+    {
+        arr[i] = temp[i-low];
+    }
+    return count;
+}
+
+int mergeSort(vector<int> &arr, int low, int high)
+{
+    int count = 0;
+    if(low >= high) return count;
+    int mid = low + (high - low) / 2;
+    count += mergeSort(arr, low, mid);
+    count += mergeSort(arr, mid + 1, high);
+    count += merge(arr, low, mid, high);
+    
+    return count;
+}
+
+class Solution {
+  public:
+    int inversionCount(vector<int> &arr) {
+        // Code Here
+        int n = arr.size();
+        return mergeSort(arr, 0, n-1);
+        
+    }
+};
