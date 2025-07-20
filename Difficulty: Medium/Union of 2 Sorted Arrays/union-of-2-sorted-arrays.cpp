42a7e2c3365e623d27060c5f96b14e92525e2fa1
Soumyadip Adhikary <145369075+Soumya1201@users.noreply.github.com>
2025-07-20 07:08:01 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.cpp
+++ Difficulty: Medium/Union of 2 Sorted Arrays/union-of-2-sorted-arrays.cpp
@@ -6,44 +6,45 @@ class Solution {
         // Your code here
         int n1 = a.size();
         int n2 = b.size();
-        int i =  0, j = 0;
-        vector<int> unionArr;
-        while(i < n1 && j < n2)
+        int i = 0, j = 0;
+        vector<int>union_arr;
+        while(i <n1 && j <n2)
         {
             if(a[i] <= b[j])
             {
-                if(unionArr.size() == 0 || unionArr.back() != a[i])
+                if(union_arr.size() == 0 || union_arr.back() != a[i])
                 {
-                    unionArr.push_back(a[i]);
+                    union_arr.push_back(a[i]);
                 }
                 i++;
             }
             else
             {
-                if(unionArr.size() == 0 || unionArr.back() != b[j])
+            
+                if(union_arr.size() == 0 || union_arr.back() != b[j])
                 {
-                    unionArr.push_back(b[j]);
+                    union_arr.push_back(b[j]);
                 }
                 j++;
             }
         }
-        while(i < n1)
+        while( i < n1)
         {
-            if(unionArr.size() == 0 || unionArr.back() != a[i])
+            if(union_arr.size() == 0 || union_arr.back() != a[i])
             {
-                unionArr.push_back(a[i]);
+                union_arr.push_back(a[i]);
             }
             i++;
         }
         while(j < n2)
         {
-            if(unionArr.size() == 0 || unionArr.back() != b[j])
+            if(union_arr.size() == 0 || union_arr.back() != b[j])
             {
-                unionArr.push_back(b[j]);
+                union_arr.push_back(b[j]);
             }
             j++;
         }
+        return union_arr;
         // return vector with correct order of elements
-        return unionArr;
     }
 };
