2670ebb2a240f5b6faf711c549544fbcf82dd80a
Soumyadip Adhikary <145369075+Soumya1201@users.noreply.github.com>
2025-07-23 08:46:29 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (93.54%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,31 +1,32 @@
-int getVal(char ch)
+int getValue(char ch)
 {
     switch(ch)
     {
-        case 'I': return 1;
-        case 'V': return 5;
-        case 'X': return 10;
-        case 'L': return 50;
-        case 'C': return 100;
-        case 'D': return 500;
-        case 'M': return 1000;
+        case 'I' : return 1;
+        case 'V' : return 5;
+        case 'X' : return 10;
+        case 'L' : return 50;
+        case 'C' : return 100;
+        case 'D' : return 500;
+        case 'M' : return 1000;
         default: return 0;
     }
 }
 class Solution {
 public:
     int romanToInt(string s) {
-        int len = s.length();
+        int n = s.length();
         int sum = 0;
-        
-        for(int i = 0; i<len; i++)
+        for(int i = 0; i<n; i++)
         {
-            char ch = s[i];
-            if(i+1 < len && getVal(ch) < getVal(s[i+1]))
+            if(i + 1 < n && getValue(s[i]) < getValue(s[i+1]))
             {
-                sum = sum - getVal(ch);
+                sum -= getValue(s[i]);
             }
-            else sum = sum + getVal(ch);
+            else
+            {
+                sum += getValue(s[i]);
+            }
         }
         return sum;
     }
