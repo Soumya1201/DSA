61a2963e2b5e30fdabf0c2ce2812f2c33c9aac6f
Soumyadip Adhikary <145369075+Soumya1201@users.noreply.github.com>
2026-01-06 21:05:02 +0530

    Time: 263 ms (14.09%) | Memory: 25.8 MB (11.41%) - LeetSync

---
+++ 940-fruit-into-baskets/fruit-into-baskets.py
@@ -0,0 +1,29 @@
+class Solution:
+    def totalFruit(self, fruits: List[int]) -> int:
+        
+        n = len(fruits)
+
+        low  = high = 0
+        res = float('-inf')
+        
+        freq = {}
+
+        for high in range(n):
+
+            freq[fruits[high]] = freq.get(fruits[high], 0) + 1
+
+            while len(freq) > 2:
+
+                freq[fruits[low]] -= 1
+
+                if(freq[fruits[low]] == 0):
+                    del freq[fruits[low]]
+                
+                low = low + 1
+
+            if len(freq) == 2 or  len(freq) == 1:
+                length = sum(freq.values())
+                res = max(res, length)   # 2,3 
+
+        if res == float('-inf'): return 1
+        else: return res
